44a7ca93f640eedbb7d4e881d1b24f915783ee56
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2022-03-23 14:01:32 +0500

    labels add feature added
    users can select label from dropdown
    users can search labels from dropdown
    if not found, they can add it instead

--- components/Dashboard/SaveSnippet/LabelSelect.js
+++ components/Dashboard/SaveSnippet/LabelSelect.js
@@ -5,17 +5,15 @@ import { SelectorIcon } from "@heroicons/react/solid";
 import { Add, Label } from "@mui/icons-material";
 import Button from "../../Generic/Button";
 import { selectUser } from "../../../redux/slices/userSlice";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import useAxiosPrivate from "../../../hooks/auth/useAxiosPrivate";
 import useAuth from "../../../hooks/auth/useAuth";
 import { useSnackbar } from "notistack";
+import dashify from "dashify";
 
 const LabelSelect = () => {
   const labels = useSelector(selectUser)?.labels || [];
-  const [labelsData, setLabelsData] = useState([
-    { name: "None", id: 0 },
-    ...labels,
-  ]);
+  const [labelsData, setLabelsData] = useState([]);
   const [selectedLabel, setSelectedLabel] = useState();
   const [query, setQuery] = useState("");
   const [labelFound, setLabelFound] = useState(true);
@@ -23,6 +21,34 @@ const LabelSelect = () => {
   const currentUser = useAuth();
 
   const { enqueueSnackbar } = useSnackbar();
+
+  const { mutate: fetchLabels } = useMutation(
+    async (labelIDs) => {
+      return await axiosPrivate.post("/api/v1/labels/many", labelIDs);
+    },
+    {
+      enabled: false,
+      onSuccess: (res) => {
+        console.log("Multiple labels fetch response", res);
+        const preparedData = res.data.result.map((label, i) => ({
+          name: label.name,
+          slug: label.slug,
+          id: i + 1,
+        }));
+        setLabelsData([
+          { name: "None", slug: "none", id: 0 },
+          ...res.data.result,
+        ]);
+      },
+      onError: (err) => {
+        const statusCode = err.response.status;
+        const statusText = err.response.statusText;
+        enqueueSnackbar(statusText, {
+          variant: "error",
+        });
+      },
+    }
+  );
 
   const filteredLabels =
     query === ""
@@ -43,16 +69,17 @@ const LabelSelect = () => {
     }
   };
 
-  const { mutate: updateUser } = useMutation(
+  const { mutate: postLabel } = useMutation(
     async (labelsData) => {
-      return await axiosPrivate.put(
-        `/api/v1/users/${currentUser?._id}`,
-        labelsData
-      );
+      return await axiosPrivate.post(`/api/v1/labels`, labelsData);
     },
     {
       onSuccess: (res) => {
-        console.log("User update[Label Addition] response", res);
+        console.log("Label post response", res);
+
+        if (res.status === 201 && res.data.updatedUser) {
+          fetchLabels({ ids: res.data.updatedUser.labels });
+        }
       },
       onError: (err) => {
         const statusCode = err.response.status;
@@ -64,7 +91,15 @@ const LabelSelect = () => {
     }
   );
 
-  const handleLabelAddition = () => {};
+  const handleLabelAddition = () => {
+    postLabel({
+      userID: currentUser?._id,
+      label: {
+        name: query,
+        slug: dashify(query),
+      },
+    });
+  };
 
   return (
     <div className="">
@@ -89,6 +124,9 @@ const LabelSelect = () => {
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
+            beforeEnter={() =>
+              labelsData.length < 1 && fetchLabels({ ids: labels })
+            }
             afterLeave={() => setQuery("")}
           >
             <Combobox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-backgroundContrast dark:bg-backgroundContrastDark rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
@@ -99,7 +137,7 @@ const LabelSelect = () => {
               ) : (
                 filteredLabels.map((item) => (
                   <Combobox.Option
-                    key={item.id}
+                    key={item.slug}
                     className={({ active }) =>
                       `cursor-default select-none relative py-2 pl-10 pr-4 ${
                         active
